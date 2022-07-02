c865aa9607410b85a70d0bdfcc286ca799780780
AshaAbdu <halge87@yahoo.com>
2022-07-01 22:52:38 -0500

    week1 lab part1 commit

--- src/week1VariablesAndOperations/VariablesAndOperations.java
+++ src/week1VariablesAndOperations/VariablesAndOperations.java
@@ -3,7 +3,37 @@ package week1VariablesAndOperations;
 public class VariablesAndOperations {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Create a variable to hold the quantity of available plane seats left on a flight
+		int availablePlaneSeats = 5;
+		
+		//Create a variable to hold the cost of groceries at checkout
+		double costOfGroceries = 23.64;
+		
+		//Create a variable to hold a person's middle Initial
+		char middleInitial = 'H';
+		
+		//Create a variable to hold true if it's hot outside and false if it's cold outside
+		boolean isHotOutside = false;
+		
+		//create a variable to hold a customer's first name
+		String firstName = "Asha";
+		
+		//Create a variable to hold a street address
+		String streetAddress = "120 Larkspur ln.";
+		
+		//Print all variables to the console
+		System.out.println("Available seats on the plane: " + availablePlaneSeats); // Concatenation
+		System.out.println(costOfGroceries + " is the price of the groceries.");
+		System.out.println("The person's middle initial is " + middleInitial);
+		System.out.println("It is hot outside - " + isHotOutside);
+		System.out.println("The customer's first name " + firstName);
+		System.out.println(streetAddress + " is where the customer lives.");
+		
+		//A customer booked two plane seats, remove 2 seats from the available seats variable
+		
+		
+		
 
 	}
 
