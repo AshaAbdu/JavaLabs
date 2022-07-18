29ca8c910f2b67f991d4bdd920bfe4acccdcc91b
AshaAbdu <halge87@yahoo.com>
2022-07-17 23:47:23 -0500

    Week3 lab part3

--- src/week3ArraysAndMethods/ArraysAndMethods.java
+++ src/week3ArraysAndMethods/ArraysAndMethods.java
@@ -85,11 +85,33 @@ public class ArraysAndMethods {
 		System.out.println(isStringGreaterThanNumber("Hello", 3)); //change to 6 prints false 
 		
 		//write and test a method that takes an array of string and a string and returns true if the String passed in exists in the array
-		System.out.println(doesArrayContainString(names, "Hello"));
+		System.out.println(doesArrayContainString(names, "Hello")); // if sam or any above name returns true
 		
 		//write and test a method that takes an array of int and returns the smallest number in the array
 		
+		System.out.println(findSmallestNumber(array)); 
+		
 		// write and test a method that takes an array of double and returns the average
+		
+		double[] doubles = {45.7, 30.2, 67.3};
+		System.out.println(calculateAverage(doubles));
+		
+		//write and test a method that takes an array of strings and returns an array of int where each element
+		//matches the length of the string at that position
+		
+		int[] namelengths =extractStringLength(names);
+		for (int number : namelengths) {
+			System.out.println(number);
+		}
+		
+		//write and test a method that takes an array of strings and returns true if the sum of letters for all strings with an
+		//even amount of letters is greater than the sum of those with an odd amount of letters
+		
+		System.out.println(hasMoreEvenWordCharacters(names));
+		
+		//write and test a method that takes a string and returns true if the string is a palindrome=reads the same both forward and backward
+		
+		System.out.println(isPalindrome("racecar"));
 		
 	}
 	//methods
@@ -116,5 +138,58 @@ public class ArraysAndMethods {
 	  }
 	  return false;
   }
+  
+  public static int findSmallestNumber(int[]numbers) {
+	  int smallest = numbers[0];
+	  for (int number : numbers) {
+		  if (number < smallest) {
+			  smallest = number;
+		  }
+	  }
+	  //outside for loop -once loop is finished- returns
+	  return smallest;
+  }
+  
+  public static double calculateAverage(double[] array) {
+	  double sum = 0;
+	  for (double number : array) {
+		  sum += number;
+	  }
+	  return sum / array.length;
+  }
+  
+  public static int[] extractStringLength(String[] strings) {
+	  int[] results = new int [strings.length];
+	  for (int i =0; i <strings.length; i++) {
+		  results[i] =strings[i].length();
+	  }
+	  return results;
+  }
+  
+  public static boolean hasMoreEvenWordCharacters(String[] array) {
+	  int evenLetters = 0;
+	  int oddLetters = 0;
 	  
+	  for (String string : array) {
+		  if (string.length() % 2 == 0) {
+			  evenLetters += string.length();		  
+		  } else {
+			  oddLetters += string.length();
+		  }	  
+	  }
+	  return evenLetters > oddLetters;
+  }
+  
+  public static boolean isPalindrome( String string) {
+	  //racecar
+	  //mom
+	  //wow
+	 for (int i =0; i < string.length()/2; i++) {
+		 if (string.charAt(i) != string.charAt(string.length() - i - 1)) {
+			 return false;
+		 }
+	 }
+	return true;  
+  }
+ 
 }
